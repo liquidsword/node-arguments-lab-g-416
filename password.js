1851b712f7ce2bae7f4ff79b874c84d933531553
liquidsword <audenrobertson@gmail.com>
2020-09-08 17:12:01 -0400

    creating a basic node password generator

--- password.js
+++ password.js
@@ -1,1 +1,10 @@
 // code solution here
+var generatePassword = function generatePassword(length, charset) {
+  var length = length || 8,
+      charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+      password = '';
+      for (var i = 0, n = charset.length; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * n ))
+      }
+      return password
+}
